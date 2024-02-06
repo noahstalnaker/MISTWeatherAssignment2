abd167722c820df90edbc8930f637dff9cdf7c94
MISTUser <MISTUser@lab8L4JUY>
2024-02-06 11:54:57 -0500

    Add project files.

---
+++ MISTWeatherAssignment2/Pages/weatherMain.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MISTWeatherAssignment2.Pages
+{
+    public class weatherMainModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
